5b605e6877d28482944c127d11bdd99c7905e25a
Yudai-ASANO <yuudai.asano@link-u.co.jp>
2023-04-24 11:37:06 +0900

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // デプロイ時に必要なファイルのみのCSSを作成してくれるものになる
+    // ビルド時に未使用のクラスを削除することになるので、最終的なCSSファイルのサイズが大幅に縮小されて、パフォーマンスが向上する。
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
